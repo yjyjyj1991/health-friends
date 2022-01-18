341bdd68c400535464bb94efff6b9899ec3570bd
김수호 <is_null@kakao.com>
2022-01-18 13:18:15 +0900

    [fix] package name update
    - heathfriend to healthfriend

--- Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/config/SwaggerConfig.java
+++ Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.heathfriend.healthfriend.config;
+package com.healthfriend.healthfriend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- Back-end/healthfriend/src/test/java/com/healthfriend/healthfriend/HealthfriendApplicationTests.java
+++ Back-end/healthfriend/src/test/java/com/healthfriend/healthfriend/HealthfriendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.heathfriend.healthfriend;
+package com.healthfriend.healthfriend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
