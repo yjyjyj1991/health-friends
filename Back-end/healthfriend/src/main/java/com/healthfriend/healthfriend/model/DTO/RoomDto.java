7165b3251884672af24de7c0e9025cd799ddddc0
김수호 <is_null@kakao.com>
2022-01-19 16:28:43 +0900

    Feat : 방 생성 처리

---
+++ Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/model/DTO/RoomDto.java
@@ -0,0 +1,20 @@
+package com.healthfriend.healthfriend.model.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoomDto {
+  private Integer id = null;
+  private int typeId;
+  private int userId;
+  private String title;
+  private String content;
+  private int isPublic;
+  private String password = null;
+  private String chat = null;
+  private String openTime = null;
+  private String closeTime = null;
+  private int limitUser;
+}
---
+++ Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/model/service/RoomService.java
@@ -0,0 +1,7 @@
+package com.healthfriend.healthfriend.model.service;
+
+import com.healthfriend.healthfriend.model.DTO.RoomDto;
+
+public interface RoomService {
+  public boolean addRoom(RoomDto room) throws Exception;
+}
