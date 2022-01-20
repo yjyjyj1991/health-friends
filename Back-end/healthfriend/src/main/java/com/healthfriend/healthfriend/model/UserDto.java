7be012c5ff43452efff1e8453e9f5c76f0ce2e36
김수호 <is_null@kakao.com>
2022-01-20 11:48:01 +0900

    Refactor : DTO ( int to Integer ), Select User 쿼리 수정

--- Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/model/UserDto.java
+++ Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/model/UserDto.java
@@ -6,13 +6,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserDto {
-    public int id;
-    public int purpose_id;
+    public Integer id;
+    public Integer purpose_id;
     public String email;
     public String name;
     public String nickname;
     public String password;
-    public int isWithdraw;
+    public Integer isWithdraw;
     public String withdraw_reason;
     public double weight;
     public double active_point;
