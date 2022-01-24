30837f47cd63f3f3cf48c127aec8187c1b13e29c
young jun <rubberboy@naver.com>
2022-01-24 14:08:24 +0900

    회원가입과 로그인 #1

--- Front-end/health-friends-app/src/App.js
+++ Front-end/health-friends-app/src/App.js
@@ -1,8 +1,46 @@
+import { NavLink, } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-
+    <div >
+      <nav style={{ display:'flex',justifyContent:'space-around' }}>
+        <NavLink
+          style={({ isActive }) => {
+            return {
+              display: "block",
+              margin: "1rem 0",
+              color: isActive ? "red" : ""
+            };
+          }}
+          to='/signup'
+        >
+          signup
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => {
+            return {
+              display: "block",
+              margin: "1rem 0",
+              color: isActive ? "red" : ""
+            };
+          }}
+          to='/formik'
+        >
+          formik
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => {
+            return {
+              display: "block",
+              margin: "1rem 0",
+              color: isActive ? "red" : ""
+            };
+          }}
+          to='/both'
+        >
+          both
+        </NavLink>
+      </nav>
     </div>
   );
 }
---
+++ Front-end/health-friends-app/src/routes/account/Signup1.jsx
@@ -0,0 +1,165 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+
+    // need validation
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+  
+  const handleBlur = (event) => {
+    console.log(event.target.id);
+    switch (event.target.id) {
+      case 'name':
+        console.log('this is name');
+        break;
+      case 'nickName':
+        console.log('this is nickname');
+        break;
+      
+      default:
+        break;
+    }
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="name"
+                  required
+                  fullWidth
+                  id="name"
+                  label="이름"
+                  autoFocus
+                  onBlur={handleBlur}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="nickName"
+                  label="닉네임"
+                  name="nickName"
+                  autoComplete="family-name"
+                  onBlur={handleBlur}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="이메일주소"
+                  name="email"
+                  autoComplete="email"
+                  onBlur={handleBlur}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="비밀번호"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                  onBlur={handleBlur}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="passwordConfirm"
+                  label="비밀번호 확인"
+                  type="password"
+                  id="passwordConfirm"
+                  autoComplete="new-password"
+                  onBlur={handleBlur}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
