7633ff918e27ffb296a59acf3ed65a7ed3a42a23
yjyjyj1991 <80735849+yjyjyj1991@users.noreply.github.com>
2022-01-27 17:01:49 +0900

    feat:로그인회원가입백엔드연결

--- Front-end/health-friends-app/src/index.js
+++ Front-end/health-friends-app/src/index.js
@@ -5,23 +5,10 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter } from 'react-router-dom';
-// import SignUp from './routes/account/SignUp'
-// import WithMaterialUI from './routes/account/Both'
-// // import FilterableProductTable from './routes/practice.jsx'
-// // import Buttons from './routes/practice1.jsx'
-// import SignupForm from './routes/account/Formik.jsx'
-// import Login from './routes/account/Login.jsx'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='signup' element={<SignUp />}/>
-        <Route path='login' element={<Login />}/>
-        </Routes>
-      </BrowserRouter>   */}
 
     <BrowserRouter>
       <App />
