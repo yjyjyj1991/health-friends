82ca4841e78994ea2f63ff8f8466ca4cf7481bf7
jiyeon <jylady0325@naver.com>
2022-02-04 16:44:56 +0900

    feat:roomcreate ui

--- Front-end/health-friends-app/src/room/RoomListCard.js
+++ Front-end/health-friends-app/src/room/RoomListCard.js
@@ -1,48 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, CardActions } from '@mui/material';
 import Grid from '@mui/material/Grid';
-import RoomData from './RoomData';
 import { ButtonGroup, Button } from '@material-ui/core';
+import axios from 'axios';
 
 function RoomListCard() {
 
-  let [datas, setDatas] = useState(RoomData);
+  
+    let [lists, setLists] = useState([]);
+
   const filterResult=(selectedType)=>{
-    const result = RoomData.filter(curData => curData.type === selectedType);
-    setDatas(result);
-}
+    const result = lists.data.filter(curData => curData.roomType === selectedType);
+    setLists(result);
+  }   
+
+ 
+
+  useEffect(()=>{
+    axios.get('https://i6d204.p.ssafy.io/api/rooms')
+         .then((Response)=>{
+            setLists(Response.data);
+            console.log(Response.data);                
+            })
+         .catch((error)=>{
+            console.log(error);
+            })
+  },[]);
 
   return (
       <>
         <div className= 'roomType'>
             <ButtonGroup>
-                <Button variant='outlined' size='large' style={{fontSize: '15px', backgroundColor: '#F6F2D4'}} onClick={()=>setDatas(RoomData)}>All</Button>
+                <Button variant='outlined' size='large' style={{fontSize: '15px', backgroundColor: '#F6F2D4'}} onClick={()=>setLists(lists)}>All</Button>
                 <Button variant='outlined' size='large' style={{fontSize: '15px', backgroundColor: '#F6F2D4'}} onClick={()=>filterResult('헬스')}>헬스</Button>
                 <Button variant='outlined' size='large' style={{fontSize: '15px', backgroundColor: '#F6F2D4'}} onClick={()=>filterResult('요가')}>요가</Button>
                 <Button variant='outlined' size='large' style={{fontSize: '15px', backgroundColor: '#F6F2D4'}} onClick={()=>filterResult('필라테스')}>필라테스</Button>
                 <Button variant='outlined' size='large' style={{fontSize: '15px', backgroundColor: '#F6F2D4'}} onClick={()=>filterResult('기타')}>기타</Button>
             </ButtonGroup> 
         </div>
-
-     <div className='roomList'>   
-      <Grid container >
-        
+ 
+     <div className='roomList'>    
+       <Grid container >
             {
-                datas.map((data, i)=>{
+                lists.data && lists.data.map((list, i)=>{
                     return(
                         <Grid item xs='3'>
-                            <List data= {datas[i]} i={i} key={i} />
+                            <List list= {list} key={i}/>
                         </Grid>
                     )
                 })
             }       
-        
        </Grid>
-       </div>
+      
+       </div>  
+
+       
+
       </>
   );
 }
@@ -52,25 +69,27 @@ function List(props) {
         <div>
             <Card sx={{ maxWidth: 345 }}>
                 <CardActionArea>
-                    <CardMedia
+                    {/* <CardMedia
                     component="img"
                     height="200"
-                    image={props.data.img}
+                    image={props.list.img}
                     alt="pic"
-                    />
+                    /> */}
                     <CardContent>
                     <Typography gutterBottom variant="h3" component="div">
-                        {props.data.title}
+                        {props.list.title}
                     </Typography>
                     <Typography variant="h5" color="text.secondary">
-                        {props.data.type}
+                        {props.list.roomType}
+                    </Typography>
+                    <Typography variant="h5" color="text.secondary">
+                        {props.list.host}
                     </Typography>
                     </CardContent>
                 </CardActionArea>
                 <CardActions>
                     <Button size="large" color="primary" variant='outlined' style={{fontSize: '15px'}}> 입장하기 </Button>
                     <Button size="large" color="primary" variant='outlined' style={{fontSize: '15px'}}> 상세보기 </Button>
-                    <span className='like'>❤ {props.data.like}</span>
                 </CardActions>
             </Card>
         </div>
