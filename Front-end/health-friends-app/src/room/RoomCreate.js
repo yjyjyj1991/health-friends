c47302483df2bd2e5f635aafbb03b9c0b81fd023
김수호 <is_null@kakao.com>
2022-02-08 11:22:52 +0900

    Fix : 리렌더 문제, Room 기본 POST 구조 처리

--- Front-end/health-friends-app/src/room/RoomCreate.js
+++ Front-end/health-friends-app/src/room/RoomCreate.js
@@ -1,57 +1,68 @@
 /*eslint-disable*/
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Dialog from "@material-ui/core/Dialog";
 import { DialogActions, DialogTitle, DialogContent, DialogContentText, TextField } from "@material-ui/core";
 import { Button } from "@material-ui/core";
 import axios from 'axios';
 import { post } from 'axios';
+import { BASE_URL } from '../common/Properties.js'
 
 
 const RoomCreate = (props) => {
   let [roomTitle, setRoomTitle] = useState('');
-  let [roomType, setRoomType] = useState('');
+  let [roomType, setRoomType] = useState(1);
   let [roomContent, setRoomContent] = useState('');
   let [isPublic, setIsPublic] = useState(true);
   let [password, setPassword] = useState('');
 
   const handlerFormSubmit = (e) => {
-    e.preventDefault()
-    this.addList()
-        .then((response)=>{
-          console.log(response.data);
-        })
+    console.log("handlerFormSubmit");
+    e.preventDefault();
+    addList()
+      .then((res) => {
+        console.log('do something');
+      })
+      .catch((err) => {
+        console.log(err + "ERRRRR");
+      });
 
-    if(roomTitle && roomType && roomContent ) {
+    if (roomTitle && roomType && roomContent) {
       console.log(roomTitle, roomType, roomContent)
     }
   }
 
-
   const addList = () => {
-    const url = 'https://i6d204.p.ssafy.io/api/rooms';
+    const url = BASE_URL + 'rooms';
     const formData = new FormData();
-    formData.append('roomType', 1);
+    formData.append('userId', 1);
+    formData.append('typeId', roomType);
     formData.append('title', roomTitle);
     formData.append('content', roomContent);
+    formData.append('isPublic', 1);
+    formData.append('password', null);
+    formData.append('limitUser', 4);
 
-    return post(url, formData);
-
+    var object = {}
+    formData.forEach(function (value, key) {
+      object[key] = value
+    })
+    var data = JSON.stringify(object)
+    return post(url, data, {
+      headers: { "Content-Type": `application/json` }
+    });
   }
-
- 
-
 
   return (
     <>
       <Dialog open={props.open} fullWidth>
-        <DialogActions> 
-          <Button variant="text" size='small' onClick={()=>{props.setOpen(false)}}> x </Button>
+        <DialogActions>
+          <Button variant="text" size='small' onClick={() => { props.setOpen(false) }}> x </Button>
         </DialogActions>
         <DialogTitle >방 생성하기</DialogTitle>
         <DialogContent>
           <DialogContentText >
-            <form onSubmit={() => handlerFormSubmit}>
-              <select name="roomType" onChange={(e) => setRoomType(e.target.value) }>
+            <form onSubmit={handlerFormSubmit}>
+              <select name="roomType" onChange={(e) => setRoomType(e.target.value)}>
                 <option value="">--운동 종류 선택--</option>
                 <option value="헬스">헬스</option>
                 <option value="요가">요가</option>
@@ -63,22 +74,22 @@ const RoomCreate = (props) => {
                 <option value='private' onChange={()=>setIsPublic(false)}>비공개</option>
                 <option value='public'>공개</option>
               </select> */}
-              <TextField variant='standard' fullWidth required label='방 이름' onChange={(e) => setRoomTitle(e.target.value)}/> 
-              <TextField variant='standard' fullWidth required multiline maxRows={4} label='방 상세 설명' onChange={(e) => setRoomContent(e.target.value)}/>
+              <TextField variant='standard' fullWidth required label='방 이름' onChange={(e) => setRoomTitle(e.target.value)} />
+              <TextField variant='standard' fullWidth required multiline maxRows={4} label='방 상세 설명' onChange={(e) => setRoomContent(e.target.value)} />
               {/* {
                 isPublic===false
                 ? <TextField type='password' variant='standard' fullWidth required label='비밀번호 설정' onChange={(e) => setPassword(e.target.value)}/>
                 : null
               } */}
-             
-              <DialogActions> 
+
+              <DialogActions>
                 <Button variant="outlined" type="submit"> 생성 </Button>
               </DialogActions>
             </form>
 
           </DialogContentText>
         </DialogContent>
-        
+
       </Dialog>
     </>
   );
