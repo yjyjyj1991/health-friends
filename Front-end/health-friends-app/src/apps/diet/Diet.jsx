be5e502088348f187de224c4a7420a560ee7cb00
yjyjyj1991 <80735849+yjyjyj1991@users.noreply.github.com>
2022-02-09 11:49:31 +0900

    feat/diet

--- Front-end/health-friends-app/src/apps/diet/Diet.jsx
+++ Front-end/health-friends-app/src/apps/diet/Diet.jsx
@@ -1,17 +1,22 @@
 import Dialog from '../../components/common/Dialog'
 import { Button,TextField,Typography,Grid } from '@mui/material'
+import { useState } from 'react'
 
-export default function Diet(props){
-  const {setDialog} = props
-  function openDialog(){
-    setDialog('dietGoal')
+export default function Diet(){
+  const [dialog,setDialog]=useState(false)
+  function open(){
+    setDialog(true)
+  }
+  function close(){
+    setDialog(false)
   }
 
   return (
+    <>
     <Grid container spacing={2}>
       <Grid item xs={9} />
       <Grid item xs={3}>
-        <Button variant='contained' onClick={openDialog}>목표설정</Button>
+        <Button variant='contained' onClick={open}>목표설정</Button>
       </Grid>
       <Grid item>
         
@@ -20,5 +25,7 @@ export default function Diet(props){
         
       </Grid>
     </Grid>
+    <Dialog close={close} dialog={dialog}/>
+    </>
   )
 }
