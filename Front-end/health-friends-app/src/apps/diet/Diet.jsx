3eaf98fa29e08d04ecec1acc36b1be404d162045
yjyjyj1991 <80735849+yjyjyj1991@users.noreply.github.com>
2022-02-10 13:06:59 +0900

    feat/diet

--- Front-end/health-friends-app/src/apps/diet/Diet.jsx
+++ Front-end/health-friends-app/src/apps/diet/Diet.jsx
@@ -1,6 +1,9 @@
-import Dialog from '../../components/common/Dialog'
 import { Button,TextField,Typography,Grid } from '@mui/material'
 import { useState } from 'react'
+import { Pie } from 'react-chartjs-2'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import DietDialog from './DietGoal';
+
 
 export default function Diet(){
   const [dialog,setDialog]=useState(false)
@@ -13,19 +16,50 @@ export default function Diet(){
 
   return (
     <>
-    <Grid container spacing={2}>
+    <Grid container spacing={2} marginY={3}>
       <Grid item xs={9} />
       <Grid item xs={3}>
         <Button variant='contained' onClick={open}>목표설정</Button>
       </Grid>
-      <Grid item>
-        
+      <Grid item xs={4} padding={2}>
+        <Pie data={data}/>
       </Grid>
-      <Grid item>
-        
+      <Grid item xs={4} padding={2}>
+        <Pie data={data}/>
+      </Grid>
+      <Grid item xs={4} padding={2}>
+        <Pie data={data}/>
       </Grid>
     </Grid>
-    <Dialog close={close} dialog={dialog}/>
+    <DietDialog close={close} dialog={dialog}/>
     </>
   )
 }
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+const data = {
+  labels: ['현재','남은 양',],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [20,100],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
--- Front-end/health-friends-app/src/apps/diet/DietGoal.jsx
+++ Front-end/health-friends-app/src/apps/diet/DietGoal.jsx
@@ -1,31 +1,114 @@
-import { Grid,TextField,Typography,Button } from "@mui/material"
+import { Grid,TextField,Typography,Select, MenuItem,FormControl,InputLabel } from "@mui/material"
+import { useState } from "react"
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import axios from 'axios';
+// 다이어트 할때 계산 방법
+// 단백질 : 몸무게 (파운드 값) * 1.1
+// 지방 : 몸무게 (파운드 값) * 0.3
+// 탄수화물 : (칼로리 - (단백질 *4 + 지방 * 9))/4 
+// 린메스업 할 때 계산 방법
+// 단백질 : 몸무게 (파운드 값) * 0.9
+// 지방 : 몸무게 (파운드 값) * 0.4
+// 탄수화물 : (칼로리 - (단백질 *4 + 지방 * 9))/4 
+// 유지 할때는
+// 탄수화물: (칼로리 총량의 50%) 
+// 단백질 : (칼로리 총량의 25%) 
+// 지방 : ((칼로리 총량의 * 0.25) * (4/9))
 
-export default function DietGoal(){
-  function handleChange(){
+// 유지 칼로리 구하는 법 : (몸무게(파운드 값으로) * 10 ) *(Active_Point DB에서 받아온 값(1.3~1.8))
+// 다이어트 칼로리 구하는 법 : (몸무게(파운드 값으로) * 10 ) *(Active_Point DB에서 받아온 값(1.3~1.8)) - 300
+// 린 메스업 칼로리 구하는 법 : (몸무게(파운드 값으로) * 10 ) *(Active_Point DB에서 받아온 값(1.3~1.8)) + 200
+export default function DietDialog(props) {
+  const [purpose,setPurpose]=useState(null)  
+  const [activePoint,setActivePoint]=useState(null)
+  const [weight,setWeight]=useState(null)
+  const BASE_URL='https://i6d204.p.ssafy.io/api/'
 
+  const handleClose = () => {
+    props.close();
+  };
+  function okay(){
+    axios.put(BASE_URL+'users',{
+      activePoint:activePoint,
+      id:JSON.parse(localStorage.getItem('user')).userInfo.id,
+      purposeId:purpose,
+      weight:weight,
+    })
+    .then(res=>console.log(res))
+    .catch(err => console.log(err))
+    props.close()
   }
+  function purposeChange(e){
+    setPurpose(e.target.value);
+  }
+  function weightChange(e){
+    setWeight(e.target.value);
+  }
+  function activePointChange(e){
+    setActivePoint(e.target.value);
+  }
+
   return (
-    <Grid container spacing={2} padding={2}>
-      <Grid item display='flex' justifyContent='center' xs={12}>
-        <Typography fontSize='large'>몸무게를 입력하면 적정량을 알려드립니다</Typography>
+    <Dialog open={props.dialog} onClose={handleClose}>
+      <DialogContent>
+      <Grid container spacing={2} padding={2}>
+        <Grid item display='flex' justifyContent='center' xs={12}>
+          <Typography fontSize='large'>적정섭취량을 알려드립니다</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">목표를 선택해주세요</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={purpose}
+              label="목표"
+              onChange={purposeChange}
+            >
+              <MenuItem value={1}>린매스업</MenuItem>
+              <MenuItem value={2}>벌크업</MenuItem>
+              <MenuItem value={3}>다이어트</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl fullWidth>
+          <InputLabel id="select-label">활동량을 선택해주세요</InputLabel>
+          <Select
+            labelId="select-label"
+            id="select"
+            value={activePoint}
+            label="목표"
+            onChange={activePointChange}
+          >
+            <MenuItem value={1.3}>매우 적음</MenuItem>
+            <MenuItem value={1.4}>적음</MenuItem>
+            <MenuItem value={1.5}>보통</MenuItem>
+            <MenuItem value={1.6}>활발</MenuItem>
+            <MenuItem value={1.8}>매우 활발</MenuItem>
+          </Select>
+        </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField 
+          required
+          onChange={weightChange}
+          label="몸무게"
+          name='weight'
+          fullWidth/>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <TextField 
-        size="small"
-        required
-        onChange={handleChange}
-        label="키"
-        name='height'
-        fullWidth/>
-      </Grid>
-      <Grid item xs={6}>
-        <TextField 
-        required
-        onChange={handleChange}
-        label="몸무게"
-        name='weight'
-        fullWidth/>
-      </Grid>
-    </Grid>
-  )
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>취소</Button>
+        <Button onClick={okay}>설정</Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
