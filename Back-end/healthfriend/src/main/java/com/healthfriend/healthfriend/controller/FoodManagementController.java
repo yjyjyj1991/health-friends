19a8df97fd3c854507b36433c79b79ca003597dc
김수호 <is_null@kakao.com>
2022-02-12 19:53:31 +0900

    Fix : 임포트 변경

--- Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/controller/FoodManagementController.java
+++ Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/controller/FoodManagementController.java
@@ -21,12 +21,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/foodmanagement")
