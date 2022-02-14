a101ec1a3c2dec138a1f75e2f3f22e7de71e7b40
김수호 <is_null@kakao.com>
2022-02-14 13:40:33 +0900

    Feat :  F5 비활성 / 만약 새로 고침 되면 세션 재입장

--- Front-end/health-friends-app/src/components/room/RoomMain.js
+++ Front-end/health-friends-app/src/components/room/RoomMain.js
@@ -12,9 +12,15 @@ import RoomSession from './RoomSession';
 
 const RoomMain = () => {
   const [dialog, setDialog] = useState(null)
+  let roomIdState = JSON.parse(window.localStorage.getItem('roomId'));
   let [open, setOpen] = useState(false);
-  let [roomId, setRoomId] = useState(null);
+  let [roomId, setRoomId] = useState(roomIdState === null ? null : roomIdState);
 
+  window.document.onkeydown = ((e) => {
+    if (roomId !== null && e.key === 'F5') {
+      return false;
+    }
+  });
   if (roomId == null) {
     return (
       <>
@@ -51,12 +57,12 @@ const RoomMain = () => {
   } else {
     return (
 
-        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}>
 
-          {/* <Container style={{minHeight:'100%'}}> */}
-            <RoomSession roomId={roomId} setRoomId={setRoomId} />
-          {/* </Container> */}
-        </div>
+        {/* <Container style={{minHeight:'100%'}}> */}
+        <RoomSession roomId={roomId} setRoomId={setRoomId} />
+        {/* </Container> */}
+      </div>
 
     );
   }
