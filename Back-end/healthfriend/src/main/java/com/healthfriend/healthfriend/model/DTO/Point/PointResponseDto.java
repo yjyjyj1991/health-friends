7ef48df000d165f4abb85f6320a366fe85894475
1ComputerMaster <87481266+1ComputerMaster@users.noreply.github.com>
2022-02-15 21:09:16 +0900

    fix: point select 부문과 front end 받는 부분 수정

--- Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/model/DTO/Point/PointResponseDto.java
+++ Back-end/healthfriend/src/main/java/com/healthfriend/healthfriend/model/DTO/Point/PointResponseDto.java
@@ -6,6 +6,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PointResponseDto {
-  Integer userId;
+  String nickname;
   Integer point;
 }
--- Front-end/health-friends-app/src/components/room/Rank.js
+++ Front-end/health-friends-app/src/components/room/Rank.js
@@ -1,5 +1,4 @@
 /*eslint-disable*/
-import * as React from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,43 +6,67 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import React, { useState, useEffect, Component } from "react";
+import axios from 'axios';
 
-function createData( rank, title, type) {
-  return { rank, title, type };
-}
 
-const rows = [
-  createData(1, 'Frozen yoghurt', '플레티넘'),
-  createData(2, 'Ice cream sandwich', '골드'),
-  createData(3, 'Eclair','골드' ),
-  createData(4,'Cupcake', '실버'),
-  createData(5,'Gingerbread', '브론즈'),
-];
 
-export default function BasicTable() {
+class Rank extends Component {
+
+
+
+  constructor(props) {
+    super(props);
+    this.userInfo = JSON.parse(localStorage.getItem('user'))['userInfo'];
+    this.state = {
+      point: [],
+      userId: [],
+      rows: [],
+    };
+  }
+
+  createData( rank, title, type) {
+    return { rank, title, type };
+  }
+  
+  componentDidMount() {
+    var count = 0;
+    axios
+      .get('https://i6d204.p.ssafy.io/api/point')
+      .then(({data})=>{
+         this.setState({
+           rows : data.data
+         })
+         console.log(this.userInfo);
+      })
+      .catch((Error)=>{
+         console.warn(Error);
+      })
+  };
+  render() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 250 }} aria-label="simple table">
-        <TableHead>
-          <TableRow sx={{backgroundColor:"beige", fontSize: "20"}}>
-            <TableCell align="center">순위</TableCell>
-            <TableCell align="center">닉네임</TableCell>
-            <TableCell align="center">레벨</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.title}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 }, backgroundColor: "#FEF5ED", fontSize:"20"}}
-            >
-              <TableCell align="center">{row.rank} </TableCell>
-              <TableCell align="center">{row.title}</TableCell>
-              <TableCell align="center">{row.type}</TableCell>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 250 }} aria-label="simple table">
+          <TableHead>
+            <TableRow sx={{backgroundColor:"beige", fontSize: "20"}}>
+              <TableCell align="center">유저</TableCell>
+              <TableCell align="center">점수</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+          </TableHead>
+          <TableBody>
+            {this.state.rows.map((row) => (
+              <TableRow
+                key={row.title}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 }, backgroundColor: "#FEF5ED", fontSize:"20"}}
+              >
+                <TableCell align="center">{row.nickname} </TableCell>
+                <TableCell align="center">{row.point}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
 }
+export default Rank;
