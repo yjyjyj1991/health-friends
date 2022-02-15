bb5e59f96811a39334d9bfe76940948510b24bf0
hhongjj <hhongjj3@gmail.com>
2022-02-15 15:39:36 +0900

    design:roomMain

--- Front-end/health-friends-app/src/components/room/RoomMain.js
+++ Front-end/health-friends-app/src/components/room/RoomMain.js
@@ -1,6 +1,7 @@
 /*eslint-disable*/
 import React, { useState } from 'react';
-import { Button, Container } from '@material-ui/core';
+import { Container } from '@material-ui/core';
+import { Button } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Rank from './Rank';
 import RoomListCard from './RoomListCard';
@@ -25,31 +26,18 @@ const RoomMain = () => {
     return (
       <>
         <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100%' }}>
+          {/* <AppBar /> */}
           <AppBar dialog={dialog} setDialog={setDialog} />
-          <div className='top' style={{ flex: '1' }}>
+          <div className='top' style={{ flex: '1', }}>
+
+            <div style={{ flex: '1', display: 'flex', justifyContent:'center', margin:'5rem' }}>
+              <Button size='large' style={{ fontSize:'15px', backgroundColor:'#ADC2A9', color:'white', '&:hover':{backgroundColor:'#D3E4CD',}}} onClick={() => { setOpen(true) }} >방 생성하기</Button>
+              <RoomCreate open={open} setOpen={setOpen} setRoomId={setRoomId} roomId={roomId} />
+            </div>
+
             <Container>
-              <Grid container>
-                <Grid item xs='8' className='myProfile'>
-                  <h3>나의 프로필</h3>
-                  <h4>김싸피</h4>
-                </Grid>
-
-                <Grid item xs='4' className='rankedList'>
-                  <h3> TOP5 </h3>
-                  <Rank />
-                </Grid>
-              </Grid>
+              <RoomListCard setRoomId={setRoomId} />
             </Container>
-
-
-          <div className='roomCreate' >
-            <Button variant='outlined' color='primary' size='large' style={{ fontSize: '15px' }} onClick={() => { setOpen(true) }} >방 생성하기</Button>
-            <RoomCreate open={open} setOpen={setOpen} setRoomId={setRoomId} roomId={roomId} />
-          </div>
-
-          <Container>
-            <RoomListCard setRoomId={setRoomId} />
-          </Container>
           </div>
           <Footer/>
         </div>
