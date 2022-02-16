90430830ca9a2873a726999e639f4603840ff26b
yj <rubber@naver.com>
2022-02-16 13:54:47 +0900

    fix:회원인증요구, 동적임포트

--- Front-end/health-friends-app/src/App.js
+++ Front-end/health-friends-app/src/App.js
@@ -1,6 +1,5 @@
 import Main from "./components/Main/Main";
 import Board from "./components/Board/Board";
-import RoomMain from "./components/room/RoomMain";
 // import './App.css';
 import { Routes, Route } from "react-router-dom";
 import {AuthContext,RequireAuth} from "./components/account/Auth";
@@ -13,7 +12,7 @@ const BASE_URL='https://i6d204.p.ssafy.io/api/'
 
 const Record = lazy(() => import("./apps/record/Record"));
 const Diet = lazy(() => import('./apps/diet/Diet'));
-
+const RoomMain = lazy(() => import("./components/room/RoomMain"));
 
 // 접근제한 거는법: RequireAuth로 감싸주면 됩니다. rooms 봐주세요.
 function App() {
@@ -34,7 +33,7 @@ function App() {
     <Suspense fallback={<Fragment />}>  
       <Routes>
         <Route path="/" element={<Main setDialog={setDialog}/>} />
-        <Route path="/rooms" element={<RoomMain />}/>
+        <Route path="/rooms" element={<RequireAuth><RoomMain /></RequireAuth>}/>
         <Route path="/boards" element={<Board />} />
         <Route path="/record" element={<RequireAuth><Record /></RequireAuth>} />
         <Route path="/diet" element={<RequireAuth><Diet/></RequireAuth>} />
